9b3f8547a6d2d24d344f4e576e6a70a81a372be2
Gabriel Reis Ruela <gabrielreis@vixdynamics.com.br>
2024-02-17 17:00:11 -0300

    mudando a pagina principal

--- settings/urls.py
+++ settings/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('sementes/', include('sementes.urls')),
+    path('', include('sementes.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
