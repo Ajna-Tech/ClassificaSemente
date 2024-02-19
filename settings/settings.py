a1b8ab672c272773ae36a813d014a3ac97090661
Gabriel Reis Ruela <gabrielreis@vixdynamics.com.br>
2024-02-19 09:21:55 -0300

    subindo alteração de root

--- settings/settings.py
+++ settings/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uk+ma!e^(agp^gd-v_lrxl@ucbkvz65ee$v4zl)+yrt381avnb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
